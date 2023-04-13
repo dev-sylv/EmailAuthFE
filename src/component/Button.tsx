5a77293b2158146d92231bc292ba42559b897008
nicsylvia <nicsylvia15f@gmail.com>
2023-04-13 13:23:26 -0700

    saved global button

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 
 const Button = () => {
-  return <div>Button</div>;
+  return (
+    <div>
+      <Main>
+        <Text>Button</Text>
+      </Main>
+    </div>
+  );
 };
 
 export default Button;
+
+const Main = styled.div<{ bg: string; cc: string }>`
+  width: 120px;
+  height: 80px;
+  background-color: ${({ bg }) => bg};
+  color: ${({ cc }) => cc};
+  border-radius: 3px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 360ms;
+
+  :hover {
+    cursor: pointer;
+    transform: scale(0.97);
+  }
+`;
+const Text = styled.div`
+  font-weight: bold;
+`;
